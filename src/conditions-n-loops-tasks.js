4103fb4a282bc388dc62be471bca4610748bd86a
ikclouds <ikclouds91@gmail.com>
2025-10-18 23:20:40 +0300

    feat: isPalindrome

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -229,8 +229,18 @@ function convertNumberToString(numberStr) {
  *  '0123210'   => true
  *  'qweqwe'    => false
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  let left = 0;
+  let right = str.length - 1;
+
+  while (left < right) {
+    if (str[left] !== str[right]) {
+      return false;
+    }
+    left += 1;
+    right -= 1;
+  }
+  return true;
 }
 
 /**
